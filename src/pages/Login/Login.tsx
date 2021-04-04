c032fcb38a7eab0344ce1ace6b763710a8824e8e
Yehor Pliasov <fenix4088@gmail.com>
2021-04-04 18:56:11 +0200

    Form started markup

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import {InputText} from "../../components/common/InputText/InputText";
+import {NavLink} from "react-router-dom";
+import {routes} from "../../router/routes";
+import { Button } from "../../components/common/Button/Button";
+import {Checkbox} from "../../components/common/Checkbox/Checkbox";
+import styled from "styled-components/macro";
 
-export const Login = (props: any) => {
+export const Login = () => {
     return (
         <>
-            <h1>Login test</h1>
+            <div>
+                <FormStyle>
+                    <span>Sing in</span>
+                    <InputText type={"text"}/>
+                    <InputText type={"password"}/>
+                    <NavLink to={routes.passwordRecover}>Forgot password?</NavLink>
+                    <Checkbox onChange={() => alert("Change")}>Remember me</Checkbox>
+                    <Button type={"submit"}>Sing in</Button>
+                    <NavLink to={routes.registration}>Registration</NavLink>
+                </FormStyle>
+            </div>
         </>
     );
 };
+
+const FormStyle = styled.form`
+  margin: 0 auto;
+  padding: 10px 15px;
+  width: 50%;
+  min-width: 300px;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column nowrap;
+  
+  border: 1px solid red;
+`;
