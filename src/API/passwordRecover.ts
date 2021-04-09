9ffef283997ddeaa6ec83d9c8c76bc331f9010c1
Mark Kadiner <dasistmail765@gmail.com>
2021-04-09 18:58:49 +0300

    Recover link improve

--- src/API/passwordRecover.ts
+++ src/API/passwordRecover.ts
@@ -1,7 +1,7 @@
 import { instance } from "./commonAPIData";
 
 const from = 'developer@gmail.com'
-const message = `<div>password recover link: <a href='http://localhost:3000/#/set-new-password/$token$'></a></div>`
+const message = `<div>password recover link: <a href='https://neko-back.herokuapp.com/2.0/set-new-password/$token$'>Link</a></div>`
 
 export const passRecoverApi = {
     async recover(email: { email: string }) {
