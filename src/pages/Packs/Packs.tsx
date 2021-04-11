132859349aec7a3ea295f315b71b73e0dc459be5
Yehor Pliasov <fenix4088@gmail.com>
2021-04-11 15:53:38 +0200

    Created PacksTableComp

--- src/pages/Packs/Packs.tsx
+++ src/pages/Packs/Packs.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { useAppDispatch } from "../../store/hooks";
 import { getPacks } from "./packsReducer";
-import { v1 } from "uuid";
 import styled from "styled-components/macro";
-import {routes} from "../../router/routes";
-import { NavLink } from "react-router-dom";
+import { PacksTable } from "./PacksTable";
 
 export const Packs = () => {
     const dispatch = useAppDispatch();
@@ -12,69 +10,12 @@ export const Packs = () => {
         dispatch(getPacks({ pageCount: 20, page: 2 }));
     }, [dispatch]);
 
-    const packsData = useAppSelector((state) => state.packs.packsTableData);
-    const isLoading = useAppSelector((state) => state.packs.isLoading);
-
-    const formatDate = (date: string):string => {
-        return new Date(date).toLocaleDateString("ru", {
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric"
-        })
-    }
-    console.log(packsData);
-
     return (
         <>
-            <PacksTable>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Cards count</th>
-                        <th>Updated</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {isLoading ? (
-                        <tr>
-                            <td>"Loading"</td>
-                        </tr>
-                    ) : (
-                        packsData.cardPacks.map((item) => {
-                            return (
-                                <PacksTr key={v1()}>
-                                    <td>{item.name}</td>
-                                    <td>{item.cardsCount}</td>
-                                    <td>{formatDate(item.updated)}</td>
-                                    <td>
-                                        <button>+</button>
-                                        <button>del</button>
-                                        <NavLink to={`${routes.cards}${item._id}`}>cards</NavLink>
-                                    </td>
-                                </PacksTr>
-                            );
-                        })
-                    )}
-                </tbody>
-            </PacksTable>
+            <PacksTable />
         </>
     );
 };
 
-const PacksTable = styled.table`
-    border: 1px solid black;
-    border-collapse: collapse;
-    width: 100%;
-`;
 
-const PacksTr = styled.tr`
-    td {
-        padding: 10px 5px;
-    }
 
-    &:nth-child(odd) {
-        background: ${({theme}) => theme.color.primary.main};
-        opacity: .5;
-    }
-`;
---
+++ src/pages/Packs/PacksTable.tsx
@@ -0,0 +1,92 @@
+import { useAppSelector } from "../../store/hooks";
+import { v1 } from "uuid";
+import { NavLink } from "react-router-dom";
+import { routes } from "../../router/routes";
+import React from "react";
+import styled from "styled-components/macro";
+
+export const PacksTable = () => {
+    const packsData = useAppSelector((state) => state.packs.packsTableData);
+    const isLoading = useAppSelector((state) => state.packs.isLoading);
+
+    const formatDate = (date: string): string => {
+        return new Date(date).toLocaleDateString("ru", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        });
+    };
+    return (
+        <PacksTableWrap>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Cards count</th>
+                    <th>Updated</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {isLoading ? (
+                    <tr>
+                        <td>"Loading"</td>
+                    </tr>
+                ) : (
+                    packsData.cardPacks.map((item) => {
+                        return (
+                            <PacksTr key={v1()}>
+                                <Td content={"left"}>{item.name}</Td>
+                                <Td content={"center"}>{item.cardsCount}</Td>
+                                <Td content={"center"}>{formatDate(item.updated)}</Td>
+                                <Td content={"center"}>
+                                    <button>+</button>
+                                    <button>del</button>
+                                    <NavLink to={`${routes.cards}${item._id}`}>cards</NavLink>
+                                </Td>
+                            </PacksTr>
+                        );
+                    })
+                )}
+            </tbody>
+        </PacksTableWrap>
+    );
+};
+
+const PacksTableWrap = styled.table`
+    margin: 0 auto;
+    border: 1px solid black;
+    border-collapse: collapse;
+    width: 80%;
+`;
+
+const PacksTr = styled.tr`
+    td {
+        padding: 10px 5px;
+    }
+
+    &:nth-child(odd) {
+        background: ${({ theme }) => theme.color.primary.main};
+        opacity: 0.5;
+    }
+
+    &:hover {
+        background: aliceblue;
+        cursor: pointer;
+    }
+`;
+
+type TdT = {
+    content: "left" | "center";
+};
+
+const Td = styled.td<TdT>`
+    text-align: ${(props) => props.content};
+
+    & > button, a {
+        margin-right: 10px;
+
+        &:last-child {
+            margin-right: 0;
+        }
+    }
+`;
